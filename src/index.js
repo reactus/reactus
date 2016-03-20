b4a1630781e5080cf79ffbb85c2d15e9c41539d8
Daniel Beff <daniel.beff@gmail.com>
2016-03-20 17:23:35 -0300

    remove components

--- src/index.js
+++ src/index.js
@@ -1,50 +1,10 @@
-import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
-import VideoDetail from './components/video_detail'
-const API_KEY = 'AIzaSyD_mhczbYGqYV8anHZsugqXeyA8KhF8ch4'
-
-
 
 // app
 class App extends Component {
-
-    constructor (props){
-        super(props);
-
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        };
-
-        this.videoSearch('surfboards');
-    }
-
-    videoSearch (term) {
-        YTSearch({key:API_KEY,term: term}, (videos) => {
-            this.setState({
-                videos:videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
-
     render (){
-        const videoSearch = _.debounce((term) => {this.videoSearch(term)},300)
-
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                    videos={this.state.videos}
-                />
-            </div>
-        )
+        return  <div> Hello world </div>
     }
 }
 
