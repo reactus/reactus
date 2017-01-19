2b748d29a050f87965aac0fe33bfb77aa1925dec
Daniel Beff <danielbeff@iMac-de-msprj.local>
2017-01-19 19:44:58 -0200

    Add basic settings for build scss with autoprefixer and source maps

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var frontendler = path.resolve(__dirname, "./node_modules/frontendler-sass");
+var autoprefixer = require("autoprefixer");
+var prodEnv = {"process.env.NODE_ENV": JSON.stringify('production')};
+
+var config = {
+    entry: ["./src/assets/scripts/main.js"],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "assets/scripts/[name].js",
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            {
+                use: ["babel-loader"],
+                test: /\.js$/,
+                include: path.join(__dirname, 'src')
+            }, {
+                test: /\.scss$/,
+                use: [
+                    "style-loader","css-loader","postcss-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            includePaths: [frontendler],
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin(prodEnv),
+        new HtmlWebpackPlugin({template: "./src/index.html"}),
+        new webpack.NoEmitOnErrorsPlugin()
+    ]
+}
+
+module.exports = config;
