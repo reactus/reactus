3e4e5d0c9a66483ac11c18d2d338a2f26a0f4bfd
Daniel Beff <daniel.beff@gmail.com>
2017-01-28 22:01:27 -0200

    Remove teste folder

--- test/index.js
+++
@@ -1,7 +0,0 @@
-import Reducers from "./../app/assets/scripts/reducers/index";
-
-describe("Reducers - ", function() {
-    it("lenght can not be zero", function() {
-        expect(Reducers.length).not.toBe(0);
-    });
-});
