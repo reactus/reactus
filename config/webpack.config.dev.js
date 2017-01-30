39ab79d3b3fe1da47d9cd57dcb01384db21ddafa
Daniel Beff <daniel.beff@gmail.com>
2017-01-30 11:14:27 -0200

    Better env setting and remove chalk require

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -4,8 +4,8 @@ var rules = require("./webpack.config.rules");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 var OpenBrowserPlugin = require("open-browser-webpack-plugin");
-var chalk = require('chalk');
 var port = 3000;
+var env = {"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)}
 var config = {
     devtool: "cheap-module-eval-source-map",
     entry: {
@@ -17,6 +17,7 @@ var config = {
     },
     module: rules,
     plugins: [
+        new webpack.DefinePlugin({env}),
         new FriendlyErrorsWebpackPlugin(),
         new HtmlWebpackPlugin({template: "./src/index.html", favicon: "./src/favicon.ico"}),
         new OpenBrowserPlugin({url:`http://localhost:${port}`})
